6ca14207e90cff4ff6709885469c25cd9e128f8b
marco_romeromon <marco.romero@eon.com.mx>
2021-09-07 15:52:30 -0500

    icontains

--- applications/autor/managers.py
+++ applications/autor/managers.py
@@ -4,3 +4,12 @@ class AutorManager(models.Manager):
     """ managers para el modelo autor """
     def listar_autores(self):
         return self.all()
+
+    def listar_50years(self):
+        return self.filter(edad=50)
+
+    def buscar_autor(self, kword):
+        resultado = self.filter(
+            nombre__icontains=kword
+        )
+        return resultado
