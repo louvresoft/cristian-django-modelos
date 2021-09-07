28239dbd35d9c488cba755bdeeb690918db2beaa
marco_romeromon <marco.romero@eon.com.mx>
2021-09-07 16:00:04 -0500

    sentencia or don operador Q

--- applications/autor/managers.py
+++ applications/autor/managers.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.db.models import Q
+
 class AutorManager(models.Manager):
     """ managers para el modelo autor """
     def listar_autores(self):
@@ -11,5 +13,11 @@ class AutorManager(models.Manager):
     def buscar_autor(self, kword):
         resultado = self.filter(
             nombre__icontains=kword
+        )
+        return resultado
+
+    def buscar_autor2(self, kword):
+        resultado = self.filter(
+            Q(nombre__icontains=kword) | Q(apellidos__icontains=kword)
         )
         return resultado
